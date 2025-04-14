19d136b6c54977dfaa3b98dbd6578416494e3c02
Akhilesh Paspureddi <akhil_p@Akhileshs-MacBook-Pro-2.local>
2025-04-13 22:51:50 -0500

    next.JS website attempt

---
+++ attempt2/my-portfolio/pages/contact.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Contact() {
+  return (
+    <div>
+      <Head>
+        <title>Contact</title>
+      </Head>
+      <h1>Contact</h1>
+      <p>Contact form or details...</p>
+    </div>
+  );
+}
---
+++ attempt2/my-portfolio/pages/experience.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Experience() {
+  return (
+    <div>
+      <Head>
+        <title>Experience</title>
+      </Head>
+      <h1>Experience</h1>
+      <p>Details about professional experience...</p>
+    </div>
+  );
+}
